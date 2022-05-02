c1ab72483cdfd1880b876af095b4f2a1c67420a1
demenz1107 <demenz1107@gmail.com>
2022-05-02 13:39:19 -0500

    Fin de la clase 1

--- app.js
+++ app.js
@@ -6,11 +6,29 @@ const allSections = document.querySelectorAll('.main-content');
 function PageTransitions(){
     //Button click active class
     for(let i = 0; i < sectBtn.length; i++){
-        sectBtn[i].addEventListener('click', () =>{
+        sectBtn[i].addEventListener('click', function(){
             let currentBtn = document.querySelectorAll('.active-btn');
-            currentBtn[0].classList = currentBtn[0].className.replace('active-btn', '');
-            this.className += 'active-btn'
+            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+            this.className += 'active-btn';
         })
     }
+    //Sections active class
+    allSections.addEventListener('click', (e) =>{
+        const id = e.target.dataset.id;
+        if(id){
+            //remove select from other btns
+            sectBtns.forEach((btn) =>{
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
+
+            //hide other sections
+            sections.forEach((section)=>{
+                section.classList.remove('active')
+            })
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+    })
 }
 PageTransitions();
